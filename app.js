174db87a197ea4401c57112bfe343da065aded78
QuentinEude <quentineude91@gmail.com>
2017-06-20 14:40:30 +0200

    Lol
    Lol

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express= require("express");
+var bodyParser = require('body-parser');
+
 var app = express();
 app.use(express.static("files"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post("/contact", function(req,res){
+    console.log(req.body);
+    res.send('Merci');
+});
+
 app.listen(8686,function(err){
     console.log("My app is running on port 8686");
 });
